cb6a7d4720acdd1beeb1257c6715fa2d297805a6
carlos eduardo <carlosedu0311@hotmail.com>
2023-03-02 23:04:31 -0300

    Criado componente de icone

--- src/components/gamePage/GamePage.jsx
+++ src/components/gamePage/GamePage.jsx
@@ -9,6 +9,3 @@ function GamePage () {
 }
 
 export default GamePage
-
-
-/// Coferir o Header que após a alteração, não houve mudança na centralização do título e subtítulo
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import styles from "./Header.module.css"
 import Title from "../title/Title"
 import SubTitle from "../subTitle/SubTitle"
+import Icon from "../icon/Icon"
 
 function Header () {
     return (
         <div className={styles.header}>
             <Title>Jogo da Velha</Title>
             <SubTitle>Desenvolvido por Carlos Eduardo</SubTitle>
+            <div className={styles.iconContent}>
+                <Icon iconName="github" size="40px" link="https://github.com/EduCarlos0311/"/>
+            </div>
         </div>
     )
 }
