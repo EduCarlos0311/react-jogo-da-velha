f83e8bc2ddd78f68e61b5e65ea7ca00fd7a6a5c4
carlos eduardo <carlosedu0311@hotmail.com>
2023-04-04 22:16:03 -0300

    teste sem placar

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -3,7 +3,7 @@ import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
 import GameInfo from '../gameInfo/GameInfo'
-import Score from '../score/Score'
+
 
 const winnerTable = [
     [0, 1, 2],
@@ -69,7 +69,6 @@ function Game () {
     }, [winner])
 
     return (
-        <>
             <div className = {styles.gameContent}>
                 <div className = {styles.game}>
                     {
@@ -91,8 +90,6 @@ function Game () {
                     isDraw = {draw}
                 />
             </div>
-            <Score />
-        </>
     )
 }
 
