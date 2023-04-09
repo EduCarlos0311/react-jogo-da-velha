e4fbd1cb6698633054fa854ab93b3a77de4c98c1
carlos eduardo <carlosedu0311@hotmail.com>
2023-04-09 17:09:41 -0300

    DESAFIO: Placar

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -23,6 +23,8 @@ function Game () {
     const [winner, setWinner] = useState(0)
     const [winnerLine, setWinnerLine] = useState([])
     const [draw, setDraw] = useState(false)
+    const [xWinnerTimes, setXWinnerTimes] = useState(0)
+    const [circleWinnerTimes, setCircleWinnerTimes] = useState(0)
 
     const handleClick = (pos) => {
         if (gameState[pos] === 0 && winner === 0) {
@@ -40,6 +42,11 @@ function Game () {
             if (sum === 3 || sum === -3) {
                 setWinner(sum / 3)
                 setWinnerLine(line)
+                if (sum > 0) {
+                    setCircleWinnerTimes(circleWinnerTimes + 1)
+                } else {
+                    setXWinnerTimes(xWinnerTimes + 1)
+                }
             }
         })
     }
@@ -92,7 +99,10 @@ function Game () {
                     isDraw = {draw}
                 />
             </div>
-            <Score />
+            <Score 
+                xWinnerTimes={xWinnerTimes}
+                circleWinnerTimes={circleWinnerTimes}    
+            />
         </>
     )
 }
